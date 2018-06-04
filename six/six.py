0767953b1fd57285e86d58ee012ce48eec8e6091
staubda <david.staub32@gmail.com>
2018-06-03 20:16:33 -0700

    added solution for 6.6

---
+++ six/six.py
@@ -0,0 +1,34 @@
+def reverse_words_simple(sentence):
+    words = []
+    word = ''
+    for c in sentence:
+        if c == ' ':
+            words.append(word)
+            words.append(' ')
+            word = ''
+        else:
+            word += c
+    words.append(word)
+        
+    sentence_rev = ''
+    while len(words) > 0:
+        sentence_rev += words.pop()
+        
+    return sentence_rev
+
+
+def reverse_words_prealloc(sentence):
+    end_ind = len(sentence)
+    word = ''
+    sentence_rev = [' '] * len(sentence)
+    for c in sentence:
+        if c == ' ':
+            start_ind = end_ind-len(word)
+            sentence_rev[start_ind:end_ind] = word
+            end_ind = start_ind - 1
+            word = ''
+        else:
+            word += c
+    sentence_rev[end_ind-len(word):end_ind] = word
+        
+    return ''.join(sentence_rev)
---
+++ six/six_test.py
@@ -0,0 +1,20 @@
+import six
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    
+    def setUp(self):
+        self.testcases = [
+            ('Bob likes Alice', 'Alice likes Bob'),
+        ]
+    
+    def execute_test(self, name, impl):
+        for str_orig, str_rev in self.testcases:
+            self.assertEqual(impl(str_orig), str_rev)
+    
+    def test_right1(self):
+        self.execute_test('right1', six.reverse_words_simple)
+        
+    def test_right2(self):
+        self.execute_test('right2', six.reverse_words_prealloc)
