d6dc42d2cb7de90cdf7ae9b2a9a12e6801757dc0
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-06 19:47:28 -0700

    fix ll, add empty tests

--- seven/linked_list.py
+++ seven/linked_list.py
@@ -1,18 +1,22 @@
+
 class LinkedList():
 
     def __init__(self, data):
         if len(data) == 0:
             return None
-
         self.value = data[0]
         self.next = None
-
-        if len(data) > 2:
+        if len(data) > 1:
             self.next = LinkedList(data[1:])
 
     def __str__(self):
         ans = str(self.value)
         if self.next is not None:
             ans += "->" + str(self.next)
+        return ans
 
+    def as_list(self):
+        ans = [self.value]
+        if self.next is not None:
+            ans += self.next.as_list()
         return ans
