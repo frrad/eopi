3696f59a8edc488b4267e91db54292ecad57adf1
staubda <david.staub32@gmail.com>
2018-06-10 15:24:24 -0700

    added solution to 1

---
+++ seven/one_test.py
@@ -0,0 +1,23 @@
+import linked_list
+import one_david
+import unittest
+
+
+class TestSevenThree(unittest.TestCase):
+
+    def setUp(self):
+        self.testcases = [
+            (linked_list.LinkedList([1, 2, 3, 8, 29, 1000]), 
+             linked_list.LinkedList([4, 18, 19, 72, 80, 99]), 
+             [1, 2, 3, 4, 8, 18, 19, 29, 72, 80, 99, 1000]),
+        ]
+
+    def execute_test(self, name, impl):
+        for A, B, merged in self.testcases:
+            self.assertEqual(impl(A, B).as_list(), merged)
+
+    def test_david(self):
+        self.execute_test('fred solution', one_david.merge_lists)
+
+if __name__ == '__main__':
+    unittest.main()
