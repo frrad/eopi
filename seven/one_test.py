ca753715146c2c87462e695015618d9d01425418
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-13 21:32:32 -0700

    Deduce merged automatically.

--- seven/one_test.py
+++ seven/one_test.py
@@ -7,16 +7,15 @@ class TestSevenOne(unittest.TestCase):
 
     def setUp(self):
         self.testcases = [
-            (([1, 2, 4, 7], [1, 4, 4, 5]),
-             [1, 1, 2, 4, 4, 4, 5, 7]),
-            (([1, 2, 3, 4], [5, 6, 7, 8]),
-             [1, 2, 3, 4, 5, 6, 7, 8]),
-            (([5, 6, 7, 8], [1, 2, 3, 4]),
-             [1, 2, 3, 4, 5, 6, 7, 8]),
+            ([1, 2, 4, 7], [1, 4, 4, 5]),
+            ([1, 2, 3, 4], [5, 6, 7, 8]),
+            ([5, 6, 7, 8], [1, 2, 3, 4]),
+
         ]
 
     def execute_test(self, name, impl):
-        for (x, y), merged in self.testcases:
+        for x, y in self.testcases:
+            merged = sorted(x + y)
             self.assertEqual(
                 impl(linked_list.LinkedList(x),
                      linked_list.LinkedList(y)).as_list(),
