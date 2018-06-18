cd395a2df62b73a193b91aeb6ccaae798a10bf82
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-17 23:47:38 -0700

    Fix merge-induced bugs

--- six/six_test.py
+++ six/six_test.py
@@ -1,25 +1,26 @@
 import six_ans
+import six_david
 import unittest
 
 
 class TestStringMethods(unittest.TestCase):
-    
+
     def setUp(self):
         self.testcases = [
             ('Bob likes Alice', 'Alice likes Bob'),
             ('the quick brown fox jumps over the lazy dog',
              'dog lazy the over jumps fox brown quick the'),
         ]
-    
+
     def execute_test(self, name, impl):
         for str_orig, str_rev in self.testcases:
             self.assertEqual(impl(str_orig), str_rev)
-    
+
     def test_david_simple(self):
-        self.execute_test('david_simple', six.reverse_words_simple)
-        
+        self.execute_test('david_simple', six_david.reverse_words_simple)
+
     def test_david_prealloc(self):
-        self.execute_test('david_prealloc', six.reverse_words_prealloc)
+        self.execute_test('david_prealloc', six_david.reverse_words_prealloc)
 
     def test_fred(self):
         self.execute_test(
@@ -32,8 +33,9 @@ class TestStringMethods(unittest.TestCase):
         self.execute_test('stephanie solution', six_ans.six_nieoh)
 
     def test_inplace_nieoh(self):
-        self.execute_test('stephanie in-place solution', six_ans.six_inplace_nieoh)
+        self.execute_test('stephanie in-place solution',
+                          six_ans.six_inplace_nieoh)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
