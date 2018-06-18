03a49f4653515c1a0b5cd402fdc1d2d5f2a5d082
Frederick Robinson <frederick.robinson@gmail.com>
2018-06-17 23:42:21 -0700

    Move solution to six

--- six/six_david.py
+++ six/six_david.py
@@ -9,11 +9,11 @@ def reverse_words_simple(sentence):
         else:
             word += c
     words.append(word)
-        
+
     sentence_rev = ''
     while len(words) > 0:
         sentence_rev += words.pop()
-        
+
     return sentence_rev
 
 
@@ -23,18 +23,12 @@ def reverse_words_prealloc(sentence):
     sentence_rev = [' '] * len(sentence)
     for c in sentence:
         if c == ' ':
-            start_ind = end_ind-len(word)
+            start_ind = end_ind - len(word)
             sentence_rev[start_ind:end_ind] = word
             end_ind = start_ind - 1
             word = ''
         else:
             word += c
-    sentence_rev[end_ind-len(word):end_ind] = word
-        
+    sentence_rev[end_ind - len(word):end_ind] = word
+
     return ''.join(sentence_rev)
-
-  
-def six_nieoh(s):
-    l = s.split(' ')
-    return ' '.join(reversed(l))
-
