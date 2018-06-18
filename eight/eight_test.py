56e6e12b21a032e5e23e5f8323e0d729cd952c5d
nieoh <steph@nieoh.com>
2018-06-18 00:24:17 -0700

    add broken test and non broken test

--- eight/eight_test.py
+++ eight/eight_test.py
@@ -1,6 +1,6 @@
 import unittest
 import eight_fred
-
+import eight_nieoh
 
 class TestEightEight(unittest.TestCase):
 
@@ -25,8 +25,40 @@ class TestEightEight(unittest.TestCase):
                     q.num_elts(), len(data) - i - 1
                 )
 
+            # Checks for enqing and deqing same number of stuff
+            q = impl(2)
+            datum = data[0]
+            q.enqueue(datum)
+            q.enqueue(datum)
+            q.dequeue()
+            self.assertEqual(
+                q.dequeue(), datum
+            )
+            self.assertEqual(
+                q.num_elts(), 0
+            )
+
+            # Want to check circularity enq * 3 + deq * 2
+            # Then check size but seems to break /shrug
+            q = impl(2)
+            for i, datum in enumerate(data):
+                q.enqueue(datum)
+                q.enqueue(datum)
+                q.enqueue(datum)
+                q.dequeue()
+                self.assertEqual(
+                    q.dequeue(), datum
+                )
+                self.assertEqual(
+                    q.num_elts(), i+1
+                )
+
+
     def test_fred(self):
         self.execute_test('fred solution', eight_fred.Queue)
+
+    def test_nieoh(self):
+        self.execute_test('nieoh solution', eight_nieoh.CircularQueue)
 
 
 if __name__ == '__main__':
