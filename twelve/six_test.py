3f02918743556e71ad5d4a43f193154042dd7993
Frederick Robinson <frederick.robinson@frrad.com>
2018-07-08 18:35:59 -0700

    12.6 test (#41)
    * 12.2 test.
    
    * Copy 2->6
    
    * Update six.
    
    * Unwrap;.

---
+++ twelve/six_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestTwelveSix(unittest.TestCase):
+
+    def setUp(self):
+        self.testcases = [
+            "all work and no play makes for no work no fun and no results".split(
+                " "),
+        ]
+
+        self.solns = [
+            ('placeholder solution', lambda par: 2),
+        ]
+
+    def test_all(self):
+        for name1, sol1 in self.solns:
+            for name2, sol2 in self.solns:
+                for case in self.testcases:
+                    message = 'Expected %s and %s to agree on %s. Got %s and %s.' % (
+                        name1, name2, case, sol1(case), sol2(case))
+                    self.assertEqual(
+                        sol1(case), sol2(case), message
+                    )
+
+if __name__ == '__main__':
+    unittest.main()
