880d25532e0e1d7a2c8728a7369ea89cef9c5be8
nieoh <steph@nieoh.com>
2018-07-08 18:44:57 -0700

    fixed

--- twelve/two_nieoh.py
+++ twelve/two_nieoh.py
@@ -9,12 +9,12 @@ def anon(mag, letter):
         if l in d:
             d[l] += 1
         else:
-            d[l] = 0
+            d[l] = 1
     
     for m in mag:
         if m in d:
             d[m] -= 1
-
-    if sum(d.values()) <= 0:
-        return True
-    return False
+    for val in d.values():
+        if val > 0:
+            return False
+    return True
