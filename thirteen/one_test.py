2f145e28171ff932fe32443fc69f47c718bed62a
Frederick Robinson <frederick.robinson@gmail.com>
2018-07-12 22:12:04 -0700

    tests

---
+++ thirteen/one_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class TestThirteenOne(unittest.TestCase):
+
+    def setUp(self):
+        self.testcases = [
+            (list('112467789'), list('12333')),
+        ]
+
+        self.solns = [
+            ('placeholder solution', lambda x, y:list(sorted(list(set(x + y))))),
+        ]
+
+    def test_all(self):
+        for name1, sol1 in self.solns:
+            for name2, sol2 in self.solns:
+                for case in self.testcases:
+                    message = 'Expected %s and %s to agree on %s. Got %s and %s.' % (
+                        name1, name2, case, sol1(*case), sol2(*case))
+                    self.assertEqual(
+                        sol1(*case), sol2(*case), message
+                    )
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ thirteen/two_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestTwelveOne(unittest.TestCase):
+
+    def setUp(self):
+        self.testcases = [
+            ([1, 2, 3, 'x', 'x'], [4, 5]),
+        ]
+
+        self.solns = [
+            ('placeholder solution', lambda a,
+             b:list(sorted([x for x in a if x != 'x'] + b))),
+        ]
+
+    def test_all(self):
+        for name1, sol1 in self.solns:
+            for name2, sol2 in self.solns:
+                for case in self.testcases:
+                    message = 'Expected %s and %s to agree on %s. Got %s and %s.' % (
+                        name1, name2, case, sol1(*case), sol2(*case))
+                    self.assertEqual(
+                        sol1(*case), sol2(*case), message
+                    )
+
+if __name__ == '__main__':
+    unittest.main()
