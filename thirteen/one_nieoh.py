acde3350f96d3812a5ea140c50bb6ff61e7e324a
nieoh <steph@nieoh.com>
2018-07-12 22:28:49 -0700

    fixed

--- thirteen/one_nieoh.py
+++ thirteen/one_nieoh.py
@@ -1,21 +1,22 @@
 def intersect(arr, brr):
     ret = []
+    last = ''
     if arr[0] > brr[0]:
         arr, brr = brr, arr
 
     a, b = 0, 0
-
+    
     def eof(x, y):
-        return x < len(arr) and y < len(brr)
+        ret = (x < len(arr)) and (y < len(brr))
+        return ret
 
     while a < len(arr) and b < len(brr):
-        print a, b, arr, brr 
-        while (arr[a] < brr[b]) or eof(a, b):
+        while eof(a, b) and (arr[a] < brr[b]):
             a += 1
-        while (arr[a] > brr[b]) or eof(a, b):
-            print brr[b]
+        while eof(a, b) and (arr[a] > brr[b]):
             b += 1
-        if arr[a] == brr[b] and ret[-1:] != arr[a]:
+        if eof(a,b) and arr[a] == brr[b] and last != arr[a]:
             ret.append(arr[a])
+            last = arr[a]
         a += 1
     return ret
